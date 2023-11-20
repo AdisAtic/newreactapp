21368ba8317a44c17f95f5c41d738c1d4861d136
AdisAtic <adisatic@gmail.com>
2023-11-20 23:58:31 +0100

    Kompl

---
+++ src/Komponenter/navbar.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Måste göra om allt, gjorde hela filen på min andra dator och måste föra över då jag gjorde fel med giten och inte kunder ladda upp alla filer. Tror jag hinner kompletera innan du ens hinner rätta</h1>
+      {/* Lägg till fler element här */}
+    </header>
+  );
+};
+
+export default Header;
